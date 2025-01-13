2cbc53f65abb68f567e2b40ebd34203abe0cf873
HannahLeah <Hannah.Leah@Oebb.at>
2025-01-13 08:14:14 +0100

    Added Hello there

--- GithubHelloWorld/Program.cs
+++ GithubHelloWorld/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace GithubHelloWorld
+﻿ namespace GithubHelloWorld
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello there!");
         }
     }
 }
